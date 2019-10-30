0d2d32c8b38332df08b94c98c0144507e341d400
Sho Ito <i.sho.0628@gmail.com>
2019-10-30 23:14:41 +0900

    Add static todos data

---
+++ src/Todos.svelte
@@ -0,0 +1,25 @@
+<script>
+  import TodoItem from './TodoItem.svelte';
+
+  let newTodoTitle = '';
+  let currentFilter = 'all';
+  let nextId = 4;
+
+  let todos = [
+    {
+      id: 1,
+      title: 'First todo',
+      completed: false
+    },
+    {
+      id: 2,
+      title: 'Second todo',
+      completed: false
+    },
+    {
+      id: 3,
+      title: 'Third todo',
+      completed: false
+    }
+  ];
+</script>
